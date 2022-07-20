dc2ac0d2e5b9f8107a8b949060dddeaa8f01c6b2
Monoblanco4 <sanchezasebastian@gmail.com>
2022-07-20 17:42:40 -0400

    Initial commit

---
+++ src/main/java/com/monoblanco/agrothia/AgrothiaMod.java
@@ -0,0 +1,16 @@
+package com.monoblanco.agrothia;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AgrothiaMod implements ModInitializer
+{
+
+    public static final String MOD_ID = "agrothia";
+
+    @Override
+    public void onInitialize ()
+    {
+
+    }
+
+}
